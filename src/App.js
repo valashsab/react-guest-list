30448648d76710a3aea50829a429e90fc4fa53a3
Val Sab <valerie.sabando@gmail.com>
2023-09-24 13:18:18 +0200

    Recover skeleton with working submitting with enter

--- src/App.js
+++ src/App.js
@@ -3,21 +3,17 @@ import styles from './styles.css';
 
 export default function App() {
   // const baseUrl = 'http://localhost:4000';
-  // User input first name
   const [firstName, setFirstName] = useState('');
-  // Uer input last name
   const [lastName, setLastName] = useState('');
+
   const [submitted, setSubmitted] = useState(false);
-
-  const [removeGuest, setRemoveGuest] = useState({ firstName } + { lastName });
-
   const fname = useRef('');
   const lname = useRef('');
 
   const handleSubmit = () => {
     setSubmitted(true);
-    // fname.current.value = ''; // --> error: cannot create property 'value' on string
-    // lname.current.value = ''; // --> error: cannot create property 'value' on string
+    fname.current.value = '';
+    lname.current.value = '';
   };
 
   useEffect(() => {
@@ -26,10 +22,10 @@ export default function App() {
 
       if (event.key === 'Enter') {
         event.preventDefault();
-        // setFirstName(''); --> div unterhalb wird nach submitting nicht abgebildet
-        // setLastName('');  --> div unterhalb wird nach submitting nicht abgebildet
-        fname.current.value = '';
-        lname.current.value = '';
+        // setFirstName('');
+        // setLastName('');
+        // fname.current.value = '';
+        // lname.current.value = '';
 
         handleSubmit();
       }
@@ -47,12 +43,11 @@ export default function App() {
       <header>
         <h1>Guest List</h1>
       </header>
+      <header>
+        <h1>Guest List</h1>
+      </header>
 
-      <form
-        onSubmit={handleSubmit} // after pushing the enter button the input fields are cleared
-        action="http://localhost:4000'"
-        // onSubmit={handleSubmit}
-      >
+      <form action="http://localhost:4000'">
         <label className={styles.labelName} htmlFor="firstName">
           First name<span className={styles.required}>*</span>
         </label>
@@ -66,7 +61,6 @@ export default function App() {
           onChange={(event) => setFirstName(event.currentTarget.value)}
           required
           ref={fname}
-          // autoComplete="off"
         />
         <br />
         <label className={styles.labelName} htmlFor="lastName">
@@ -82,27 +76,21 @@ export default function App() {
           onChange={(event) => setLastName(event.currentTarget.value)}
           required
           ref={lname}
-          // autoComplete="off"
         />
       </form>
-      <br />
-      <br />
+
       {submitted && (
-        <div data-test-id="guest">
-          New guest: <br />
+        <div>
+          {' '}
+          Guests <br />
           {firstName} {lastName}
-          <br />
-          <br />
           <form>
-            <label htmlFor="notAttending" data-bind="checked">
-              Not Attending
-            </label>
+            <label htmlFor="notAttending">Not Attending</label>
             <input
               type="checkbox"
               id="notAttending"
               name="notAttending"
               value="notAttending"
-              checked
             />
             <br />
             <label htmlFor="Attending">Attending</label>
@@ -113,11 +101,11 @@ export default function App() {
               value="Attending"
             />
           </form>
-          <br />
-          <br />
-          <button onClick={() => setRemoveGuest(newGuests.pop)}>Remove</button>
         </div>
       )}
+      <br />
+      <br />
+      <button>Remove</button>
     </>
   );
 }
